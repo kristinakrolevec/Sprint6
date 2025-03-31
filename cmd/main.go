06da522848dd2c70ae71ca5167aaf5d8ef9966c5
Кристина Кролевец <kristinakrolevec@yandex.ru>
2025-03-31 20:45:15 +0300

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
 func main() {
+	//Здесь, в функции main() нужно создать логгер,
+	// далее создать сервер с помощью вашей функции из пакета server,
+	// и запустить его. Если при запуске сервера возникают ошибки,
+	// выведите её с помощью логгера на уровне Fatal.
+
+	file, err := os.OpenFile("info.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	myLog := log.New(file, "serv", log.LstdFlags|log.Lshortfile)
+
+	srv := server.MyServer(myLog)
+
+	err = srv.Serv.ListenAndServe()
+	if err != nil {
+		log.Fatal()
+	}
 
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,26 @@
 package service
+
+import "github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+
+func ConvMorseString(morseOrString string) string {
+	var formatMorse bool
+	var resConv string
+	for _, v := range morseOrString {
+		if v == 45 || v == 46 || v == 32 {
+			formatMorse = true
+		} else {
+			formatMorse = false
+			break
+		}
+	}
+	if formatMorse {
+
+		// ToText converts a morse string to his textual representation, it is an alias to DefaultConverter.ToText.
+		resConv = morse.ToText(morseOrString)
+	}
+	if !formatMorse {
+		// ToMorse converts a text to his morse rrpresentation, it is an alias to DefaultConverter.ToMorse.
+		resConv = morse.ToMorse(morseOrString)
+	}
+	return resConv
+}
